7e476def4cb968714b82a667a47291ebb621fa0b
tanzeeldilaramkhan21 <tanzeelkhandec21@gmail.com>
2024-09-21 22:52:52 +0500

    done with post request in postman

--- index.mjs
+++ index.mjs
@@ -22,11 +22,15 @@ app.get('/api/users', (req, res) => {
 // POST request - Create a new item
 app.post('/api/users' , (req, res) => {
     const newItem = {
-        id: users.length + 1,
-        name: req.body.name
-    };
+    //     id: users.length + 1,
+    //     name: req.body.name,
+    //     email: req.body.email
+    // };
+    // we can also write as
+    id: users.length + 1, // Assuming you're using users instead of items
+    ...req.body };// Spread the request body to include name and email
    users.push(newItem);
-    res.status(201).json({message: 'POST request - new item is created' , data:  users});
+    res.status(201).json({message: 'POST request - new item is created' , data: newItem});
 });
 
 
