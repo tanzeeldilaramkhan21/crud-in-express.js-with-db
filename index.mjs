622f48579cdff6edf06c9a6e6b4bcdfce3c2b26a
tanzeeldilaramkhan21 <tanzeelkhandec21@gmail.com>
2024-09-21 21:59:48 +0500

    get request

--- index.mjs
+++ index.mjs
@@ -4,9 +4,23 @@ const app = express()
 
 // json middleware
 app.use(express.json())
+
+// array of user's data
+let users = [
+    {id : 1, name: "saba", email: "saba12@gmail.com"},
+    {id : 2, name: "aqsa", email: "aqsa12@gmail.com" },
+    {id : 3, name: "samia", email: "samia12@gmail.com"}
+];
+
  
 //GET request or get all items
 
-app.get('/api/items', (req, res) => {
+app.get('/api/users', (req, res) => {
     res.status(200).json({ message: 'GET request - Fetching all items' });
+})
+
+
+// start the server
+app.listen(PORT, () => {
+    console.log(`server running on http://localhost:${PORT}`);
 })
