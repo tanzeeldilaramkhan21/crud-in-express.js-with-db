0c04fd638cd3ffa947fadb2e82f6f48147777eb2
tanzeeldilaramkhan21 <tanzeelkhandec21@gmail.com>
2024-09-21 15:01:50 +0500

    get

--- index.mjs
+++ index.mjs
@@ -4,3 +4,9 @@ const app = express()
 
 // json middleware
 app.use(express.json())
+ 
+//GET request or get all items
+
+app.get('/api/items', (req, res) => {
+    res.status(200).json({ message: 'GET request - Fetching all items' });
+})
